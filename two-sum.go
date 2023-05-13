8dc623ce789502e4cf1e7cff4bbb37f6b79f5c39
adipginting <apg.foss@gmail.com>
2023-05-13 17:17:23 +0700

    Change two sum to golang

---
+++ two-sum.go
@@ -0,0 +1,16 @@
+import "fmt"
+func twoSum(nums []int, target int) []int {
+    var hashmap map[int]int
+    hashmap = make(map[int]int)
+    arr := []int{0, 0}
+    for i:= 0; i < len(nums); i++{
+      value, ok := hashmap[target - nums[i]]
+      if ok {
+        arr[0] = value
+        arr[1] = i
+      } else {
+        hashmap[nums[i]] = i
+      }
+    }
+    return arr
+}
