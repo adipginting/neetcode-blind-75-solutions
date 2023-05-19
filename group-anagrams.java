b979f541c590bceca19180837faff0e5a88ece8f
adipginting <apg.foss@gmail.com>
2023-05-19 16:24:26 +0700

    fix: add some conditions and 57/118 tests passeds

--- group-anagrams.java
+++ group-anagrams.java
@@ -2,6 +2,14 @@ import java.util.HashMap;
 import java.util.List;
 class Solution {
   public List<List<String>> groupAnagrams(String[] strs) {
+    if (strs.length == 1){
+      ArrayList<ArrayList<String>> listOfList = new ArrayList<ArrayList<String>>();
+      ArrayList<String> listOfString = new ArrayList<String>();
+      listOfString.add(strs[0]);
+      listOfList.add(listOfString);
+      return (List<List<String>>) (List<?>) listOfList;
+    }
+
     HashMap<Integer, ArrayList<String>> map = new HashMap<Integer, ArrayList<String>>();
     for (int i = 0; i < strs.length; i++){
       int integerValue = 0;
@@ -10,10 +18,19 @@ class Solution {
       }
       ArrayList<String> list = new ArrayList<String>();
       if (map.containsKey(integerValue)){
-        list = map.get(integerValue);
-        list.add(strs[i]);
-        map.put(integerValue, list);
-      } else {
+        if (integerValue == 0){
+          list = map.get(integerValue);
+          list.add(strs[i]);
+          map.put(integerValue, list);
+        } else if (map.get(integerValue).get(0).contains(strs[i].substring(0,1))){
+          list = map.get(integerValue);
+          list.add(strs[i]);
+          map.put(integerValue, list);
+        } else{
+          list.add(strs[i]);
+          map.put(integerValue + 2847, list);
+        }
+      }else {
         list.add(strs[i]);
         map.put(integerValue, list);
       }
