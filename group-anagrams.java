71924513bc3b1a1d5af9dfad1644531a1e52659f
adipginting <apg.foss@gmail.com>
2023-05-25 07:17:40 +0700

    fix: group anagrams with built in sort

--- group-anagrams.java
+++ group-anagrams.java
@@ -1,46 +1,28 @@
 import java.util.HashMap;
 import java.util.List;
+
 class Solution {
   public List<List<String>> groupAnagrams(String[] strs) {
-    if (strs.length == 1){
-      ArrayList<ArrayList<String>> listOfList = new ArrayList<ArrayList<String>>();
-      ArrayList<String> listOfString = new ArrayList<String>();
-      listOfString.add(strs[0]);
-      listOfList.add(listOfString);
-      return (List<List<String>>) (List<?>) listOfList;
-    }
-
-    HashMap<Integer, ArrayList<String>> map = new HashMap<Integer, ArrayList<String>>();
-    for (int i = 0; i < strs.length; i++){
-      int integerValue = 0;
-      for (int j = 0; j < strs[i].length(); j++){
-        integerValue = integerValue + strs[i].charAt(j);
-      }
-      ArrayList<String> list = new ArrayList<String>();
-      if (map.containsKey(integerValue)){
-        if (integerValue == 0){
-          list = map.get(integerValue);
-          list.add(strs[i]);
-          map.put(integerValue, list);
-        } else if (map.get(integerValue).get(0).contains(strs[i].substring(0,1))){
-          list = map.get(integerValue);
-          list.add(strs[i]);
-          map.put(integerValue, list);
-        } else{
-          list.add(strs[i]);
-          map.put(integerValue + 2847, list);
-        }
-      }else {
-        list.add(strs[i]);
-        map.put(integerValue, list);
+    // convert the string as key
+    // create new List with number as its key
+    // add every value inside the hashmap to a new List
+    HashMap<String, List<String>> map = new HashMap<>();
+    List<List<String>> result = new ArrayList<>();
+    for (String str : strs) {
+      char[] charArr = str.toCharArray();
+      Arrays.sort(charArr);
+      String orderedStr = String.valueOf(charArr);
+      if (map.containsKey(orderedStr)) {
+        map.get(orderedStr).add(str);
+      } else {
+        List<String> strList = new ArrayList<>();
+        strList.add(str);
+        map.put(orderedStr, strList);
       }
     }
-
-    ArrayList<ArrayList<String>> returnValue = new ArrayList<ArrayList<String>>();
-    for(Integer key: map.keySet()){
-      returnValue.add(map.get(key));
+    for (String key : map.keySet()) {
+      result.add(map.get(key));
     }
-
-    return (List<List<String>>) (List<?>) returnValue;
+    return result;
   }
 }
