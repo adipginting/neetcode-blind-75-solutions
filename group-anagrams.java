cffb0e54317b46998d175f87af0d8fe3f02b7b43
Adi Primanda Ginting <adipginting@users.noreply.github.com>
2023-05-25 15:00:08 +0700

    Update group-anagrams.java
    Adding step description at the top of the file

--- group-anagrams.java
+++ group-anagrams.java
@@ -3,9 +3,12 @@ import java.util.List;
 
 class Solution {
   public List<List<String>> groupAnagrams(String[] strs) {
-    // convert the string as key
-    // create new List with number as its key
-    // add every value inside the hashmap to a new List
+    // Steps:
+    // 1. convert the strings to array of characters
+    // 2. sort the characters, convert the ordered characters back to string, and use it as a key in a hashmap
+    // 3. check whether each string exists inside the hashmap and add them using list of string
+    // 4. put all of the content of the anagrams and non anagrams to a list 
+    add every value inside the hashmap to a new List
     HashMap<String, List<String>> map = new HashMap<>();
     List<List<String>> result = new ArrayList<>();
     for (String str : strs) {
